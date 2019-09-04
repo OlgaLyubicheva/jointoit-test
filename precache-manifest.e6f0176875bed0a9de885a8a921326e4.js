af48a553764c027ccb491daed66608af61b0f742
Olga Lyubicheva <forjobslov@gmail.com>
2019-09-04 10:10:05 +0300

    Updates

---
+++ precache-manifest.e6f0176875bed0a9de885a8a921326e4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b81ed51af9fc29f9978a82718cf89e9a",
+    "url": "/jointoit-test/index.html"
+  },
+  {
+    "revision": "f03701c3a08f6be36f02",
+    "url": "/jointoit-test/static/css/2.f997c740.chunk.css"
+  },
+  {
+    "revision": "afea2ce11ba36d3b5d2d",
+    "url": "/jointoit-test/static/css/main.d3a82248.chunk.css"
+  },
+  {
+    "revision": "f03701c3a08f6be36f02",
+    "url": "/jointoit-test/static/js/2.5edde915.chunk.js"
+  },
+  {
+    "revision": "afea2ce11ba36d3b5d2d",
+    "url": "/jointoit-test/static/js/main.e6128370.chunk.js"
+  },
+  {
+    "revision": "26dc343c0bbc6084158f",
+    "url": "/jointoit-test/static/js/runtime~main.907e78cf.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/jointoit-test/precache-manifest.a283bfbd54e17c621d40dec42771c138.js"
+  "/jointoit-test/precache-manifest.e6f0176875bed0a9de885a8a921326e4.js"
 );
 
 self.addEventListener('message', (event) => {
